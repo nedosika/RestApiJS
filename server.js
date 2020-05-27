d73561978c484285a9582b597182b589a90d7b37
root <root@inspiron.nedosika.pp.ua>
2020-05-27 15:54:08 +0300

    add DELETE, POST and  validate

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const Hapi = require('@hapi/hapi');
 const MySQL = require('mysql');
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
 
 const server = new Hapi.server({
     port: 3000,
@@ -12,7 +12,7 @@ const server = new Hapi.server({
 const connection = MySQL.createConnection({
     host: '192.168.0.200',
     user: 'nodejs',
-    password: '5BAaDON9f3stNdp5',
+    password: 'lg23GrT48aClbUfW',
     database: 'nodejs'
 });
 
@@ -64,6 +64,77 @@ server.route({
     handler: (request, h) => {
 	return 'Hello World!';
     }
+});
+
+server.route({
+    method: 'POST',
+    path: '/messages',
+    handler: async (request, reply) =>  {
+        return new Promise((resolve, reject) => {
+            const uid = request.payload.uid;
+            connection.query('SELECT * FROM messages WHERE uid_fk = "' + uid + '"', (error, results, fields) => {
+                if (error)
+                    reject(error)
+                else {
+                    resolve(results)
+                }
+            });
+        });
+    },
+
+//    config: {
+//        cors: {
+//    	    origin: ['*'],
+//            additionalHeaders: ['cache-control', 'x-requested-with']
+//        },
+//        validate: {
+//            payload: {
+//                uid: Joi.number().integer()
+//            }
+//        }
+//    },
+    options: {
+         cors: {
+             origin: ['*'],
+             additionalHeaders: ['cache-control', 'x-requested-with']
+        },
+	validate: {
+	    payload: Joi.object({
+		uid: Joi.number().integer()
+	    })
+	}
+    }
+});
+
+server.route({
+    method: 'DELETE',
+    path: '/message/{uid}/{mid}',
+    handler: async (request, reply) =>  {
+        return new Promise((resolve, reject) => {
+    	    const uid = request.params.uid;
+    	    const mid = request.params.mid;
+    	    const queryString = 'DELETE FROM messages WHERE uid_fk = "' + uid + '" AND mid = "' + mid + '"';
+    	    connection.query(queryString, (error, results, fields) => {
+    	        if (error)
+    		    reject(error)
+    	        else {
+    	    	    if (results.affectedRows)
+    	    		resolve(true)
+    	    	    else {
+    	    		resolve(false)
+    	    	    }
+		}
+    	    });
+    	})
+    },
+//    config: {
+//        validate: {
+//            params: {
+//                uid: Joi.number().integer(),
+//                mid: Joi.number().integer()
+//            }
+//        }
+//    }
 });
 
 server.start();
